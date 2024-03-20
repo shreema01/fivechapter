fb931253f969d2d870757ded95837256db2c97db
Shreema Dey Purba <122149624+shreema01@users.noreply.github.com>
2024-03-20 13:27:59 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-//comments
 public class Main {
-      /**
-  * this is my java docx example
-  *
-  *
-    public static void main(String[] args)
- */
-      public static void main(String[] args)
-      {
-
-        //this is important,don't delect this(single line comments)
-        /*
-        this is important,don't delect this
-        (multiple line comments)
-         */
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Shreema's Journey");
     }
 }
